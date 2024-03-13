358732c3ade752977d9fd271f30829d1e713a2a8
ambareeshkumar <ambareeshkumar10@gmail.com>
2024-03-13 06:58:49 +0530

    Finding maximum number of strings pairs using dict

--- Leetcode/Intersection of two Arrays.py
+++ Leetcode/Intersection of two Arrays.py
@@ -11,7 +11,17 @@ class Solution:
                     res.append(ele)
             return res
 
+    """
+    One linear solution for this problem
+    Solution 1
+    return [ele for ele in set(nums1) if ele in set(nums2)]
+    
+    Solution 2
+    return list(set(nums1) & set(nums2))
+    """
+
 sol = Solution()
 nums1 = [1,2,2,1]
 nums2 = [1,2,2,1]
 print(sol.intersection(nums1,nums2))
+
